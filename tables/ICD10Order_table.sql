f7e9cccfa708ea66064c0d6ed04c15d3149e58de
Zion Dials <me@ziondials.com>
2018-11-05 13:31:55 -0500

    Update ICD10Order_table.sql

--- tables/ICD10Order_table.sql
+++ tables/ICD10Order_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS "ICD10Order"
   "ICD10Code"         VARCHAR(7)    NOT NULL,
   "Header"            INTEGER       NOT NULL,
   "Short_Description" VARCHAR(60)   NOT NULL,
-  "Long_Description"  VARCHAR(3000) NOT NULL,
+  "Long_Description"  VARCHAR(3000) NOT NULL
 );
 
 ALTER TABLE "ICD10Order" OWNER TO "postgres";
